1e176f26f06e58834f1be45927a9882acff99496
chappse6 <chappse6@gmail.com>
2022-10-30 13:15:07 +0900

    Initial commit

---
+++ src/main/java/com/example/jwttutorial/repository/AuthorityRepository.java
@@ -0,0 +1,8 @@
+package com.example.jwttutorial.repository;
+
+import com.example.jwttutorial.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+
+}
